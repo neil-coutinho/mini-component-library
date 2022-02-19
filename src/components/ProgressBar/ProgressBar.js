b13944c513968b57b74b36d72dc0cefc5477c462
Neil Coutinho <njcoutinho@gmail.com>
2022-02-19 09:03:00 +0530

    Add <div> with role = progressbar and aria-values

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,16 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+
+const ProgressbarStyles = `
+
+
+
+`;
+
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return <div role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100">{value}%</div>
 };
 
 export default ProgressBar;
