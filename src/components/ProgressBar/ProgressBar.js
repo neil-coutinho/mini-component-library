f12aea2404223760d9aec69bde907002c3225f65
Neil Coutinho <njcoutinho@gmail.com>
2022-02-19 09:38:09 +0530

    added styles object with height and padding config based on size

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,27 +5,47 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const styles = {
+  small: {
+    height: '8px',
+    padding: 0,
+  },
+  medium: {
+    height: '12px',
+    padding: 0,
+  },
+  large: {
+    height: '16px',
+    padding: '4px'
+  }
+}
 
-const ProgressbarStyles = styled.div`
-  box-shadow: inset 0 2px 4px ${COLORS.transparentGray15};
-  background: ${COLORS.transparentGray15};
-  
-
-`;
-
-const BarStyles = styled.div`
-  background: ${COLORS.primary};
-  width: ${({width}) => `${width}%`};
-  height: 8px;
-  
-`;
-
-
+// size = ['small', 'medium', 'large'],
 const ProgressBar = ({ value, size }) => {
-  return <ProgressbarStyles role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100">
+
+  const style = styles[size];
+
+  return <ProgressbarStyles role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100" style={{'--padding': style.padding}}>
     {/* {value}% */}
-      <BarStyles width={value}></BarStyles>
+      <BarStyles width={value} style={{'--height': style.height}}></BarStyles>
     </ProgressbarStyles>
 };
 
+  const ProgressbarStyles = styled.div`
+    box-shadow: inset 0 2px 4px ${COLORS.transparentGray15};
+    background: ${COLORS.transparentGray15};
+    border-radius: 4px;
+    padding: var(--padding);
+   
+    
+  
+  `;
+  
+  const BarStyles = styled.div`
+    background: ${COLORS.primary};
+    width: ${({width}) => `${width}%`};
+    height: var(--height);
+   
+    
+  `;
 export default ProgressBar;
