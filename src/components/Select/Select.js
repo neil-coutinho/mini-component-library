4bb7412c6489b49a533b969c47cf24ece6450e2c
Neil Coutinho <njcoutinho@gmail.com>
2022-02-20 12:30:50 +0530

    added base layout and styles

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,41 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+
+
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
+  const Wrapper = styled.div`
+    background-color: ${COLORS.transparentGray15};
+    border-radius: 8px;
+    color: ${COLORS.gray700};
+    padding: 12px 16px;
+  
+  `;
+
+  const Select = styled.select`
+  
+  
+  `;
+
+  const CustomSelect = styled.div`
+
+  `;
+
+
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <CustomSelect>
+        {displayedValue}
+      </CustomSelect>
+       <Select value={value} onChange={onChange}>
+        {children}
+      </Select>
+
+    </Wrapper>
+   
   );
 };
 
