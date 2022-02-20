d7d93c685cf15fdda69236888beb70df04924146
Neil Coutinho <njcoutinho@gmail.com>
2022-02-20 13:24:01 +0530

    outline and focus styles

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -15,11 +15,12 @@ const Select = ({ label, value, onChange, children }) => {
     background-color: ${COLORS.transparentGray15};
     border-radius: 8px;
     color: ${COLORS.gray700};
-    padding: 12px 16px;
     width: max-content;
     position: relative;
-  
-  `;
+    padding: 12px 16px;
+    padding-right: 24px;
+    
+    `;
 
   const Select = styled.select`
     position: absolute;
@@ -27,22 +28,45 @@ const Select = ({ label, value, onChange, children }) => {
     top: 0;
     bottom: 0;
     opacity: 0;
-  
-  `;
+    width: 100%;
+    
+    
+    `;
 
   const CustomSelect = styled.div`
+    padding-right: 16px;
 
+    ${Select}:focus + & { 
+      outline-color: #4374CB;
+      -webkit-outline: 2px solid #4374CB;
+    }
+
+    ${Select}:hover + & { 
+      color: ${COLORS.black}
+    }
   `;
+
+  const IconStyles = styled(Icon)`
+    position: absolute;
+    right: 10px;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    pointer-events: none;
+    font-size: 1rem;
+  `; 
 
 
   return (
     <Wrapper>
-      <CustomSelect>
-        {displayedValue}
-      </CustomSelect>
+     
        <Select value={value} onChange={onChange}>
         {children}
       </Select>
+      <CustomSelect>
+        {displayedValue}
+        <IconStyles id="chevron-down" size={16}></IconStyles>
+      </CustomSelect>
 
     </Wrapper>
    
